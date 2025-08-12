55fe747208a308063e9784a61e4ed86e1e499c53
Eazybel <eazybel27@gmail.com>
2025-08-12 17:32:22 +0300

    delete 1048 files

--- 2-how-node-works/final/modules.js
+++
@@ -1,5 +0,0 @@
-const adder=require("./test-module-2.js")
-
-console.log(adder.add(6,7))
-console.log(adder.multiply(6,7))
-console.log(adder.divide(6,7))
--- 2-how-node-works/starter/event-loop.js
+++
@@ -1,9 +0,0 @@
-const fs=require("fs")
-setTimeout(()=>{
-console.log("hello timeout")
-},0)
-setImmediate(()=>{console.log("helloo immediate")})
-fs.readFile("./test-file.txt","utf-8",(err,req)=>{
-console.log("file readed I/O")
-})
-console.log("hello from the top level")
